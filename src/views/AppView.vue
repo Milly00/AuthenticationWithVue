300e0b24b5eb71ead5bbb0412826c4351aafb426
Milly <benitezmilly3@gmail.com>
2025-04-15 21:04:19 -0500

    first comment, project with bootstrap

---
+++ src/views/AppView.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>App Vue</h1>
+  <div class="container">
+    <form class="row">
+      <div class="form-group">
+        <div class="form-floating mb-3">
+          <input
+            type="email"
+            class="form-control"
+            v-model="email"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="password"
+            v-model="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+      </div>
+      <div>
+        <button type="button" class="btn btn-success mt-2">Success</button>
+      </div>
+    </form>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+let email = ref("");
+let password = ref("");
+</script>
+
+<style></style>
